e71bb9a7c5314d14129a3dc8d33433022cee61a2
Travis Johnson <travis@thisguy.codes>
2016-03-23 16:33:09 -0700

    I put my name on it

---
+++ tower/tower.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/trumptowers/trump/tower/deflect"
+	"github.com/trumptowers/trump/tower/hands"
+	"github.com/trumptowers/trump/tower/makeamericagreatagain"
+)
+
+func main() {
+	db := hands.New()
+	reg := theleastracist.NewRegistrar(db)
+	deflector := deflect.New(db)
+
+	regMux := http.NewServeMux()
+
+	regMux.Handle("/register", reg)
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":80", deflector))
+	}()
+	log.Fatal(http.ListenAndServe(":2016", regMux))
+}
