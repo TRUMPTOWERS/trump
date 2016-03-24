0cd1619fc42a4a9d195ee2b8826d7a56b2d56465
Travis Johnson <travis@thisguy.codes>
2016-03-23 21:42:24 -0700

    checkpoint

--- tower/hands/hands.go
+++ tower/hands/hands.go
@@ -3,25 +3,35 @@ package hands
 import (
 	"log"
 	"sync"
+	"time"
 )
 
-// DB reprsents an interface to a domain:address database
+type dbEntry struct {
+	hostPort  string
+	timestamp time.Time
+}
+
+// DB represents an interface to a domain:address database
 type DB struct {
-	db map[string]string
+	db       map[string]dbEntry
+	duration time.Duration
 	sync.Mutex
 }
 
 // New creates a DB
 func New() *DB {
-	thisDB := &DB{}
-	thisDB.db = make(map[string]string)
+	thisDB := &DB{
+		db:       make(map[string]dbEntry),
+		duration: time.Duration(24 * time.Hour),
+	}
 	return thisDB
 }
 
 // Set adds/replaces a value in the DB
 func (db *DB) Set(domain string, hostPort string) {
 	db.Lock()
-	db.db[domain] = hostPort
+	entry := dbEntry{hostPort: hostPort, timestamp: time.Now()}
+	db.db[domain] = entry
 	db.Unlock()
 	log.Printf("Saved %q for use with %q\n", hostPort, domain)
 }
@@ -29,8 +39,41 @@ func (db *DB) Set(domain string, hostPort string) {
 // Get retrives a value from the DB, or (nil,0) if it doesn't exist
 func (db *DB) Get(domain string) string {
 	db.Lock()
-	thisAddress := db.db[domain]
+	thisAddress := db.db[domain].hostPort
+	if time.Since(db.db[domain].timestamp) > db.duration {
+		delete(db.db, domain)
+		thisAddress = ""
+	}
 	db.Unlock()
 	log.Printf("Got %q for requested %q\n", thisAddress, domain)
 	return thisAddress
+}
+
+// GetTimestamp retreives the time that an entry was added
+func (db *DB) GetTimestamp(domain string) time.Time {
+	db.Lock()
+	time := db.db[domain].timestamp
+	db.Unlock()
+	return time
+}
+
+// GetAll retreives all entries in the database
+func (db *DB) GetAll() []string {
+	db.Lock()
+	var all []string
+	var expired []string
+
+	for k, v := range db.db {
+		if time.Since(v.timestamp) > db.duration {
+			expired = append(expired, k)
+		} else {
+			all = append(all, k)
+		}
+	}
+
+	for _, s := range expired {
+		delete(db.db, s)
+	}
+	db.Unlock()
+	return all
 }
--- tower/hands/hands_test.go
+++ tower/hands/hands_test.go
@@ -2,6 +2,7 @@ package hands_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/trumptowers/trump/tower/hands"
 )
@@ -17,6 +18,36 @@ func TestGetHostPort(t *testing.T) {
 
 	if gotIP != thisIP {
 		t.Fatalf("Mismatched HostPort, expected %q, got %q\n", thisIP, gotIP)
+	}
+}
+
+func TestGetTimestamp(t *testing.T) {
+	t.Parallel()
+	mine := hands.New()
+
+	mine.Set("trump", "127.5.6.250:2016")
+
+	timestamp := mine.GetTimestamp("trump")
+	if time.Since(timestamp).Seconds() > 1 {
+		t.Fatalf("Incorrect timestamp")
+	}
+}
+
+func TestGetExpired(t *testing.T) {
+	t.Parallel()
+}
+
+func TestGetAll(t *testing.T) {
+	t.Parallel()
+	mine := hands.New()
+
+	mine.Set("trump", "127.5.6.250:2016")
+	mine.Set("drumpf", "127.0.0.1:2020")
+
+	all := mine.GetAll()
+
+	if !(all[0] == "trump" && all[1] == "drumpf") {
+		t.Fatalf("failed to retreive all entries")
 	}
 }
 
